7cc3f0dfcda457c38f20f533a963afda06187df4
swapnil takalkare <swapniltakalkar1234@gmail.com>
2024-06-01 13:19:27 +0530

    Done !!

---
+++ src/components/About.jsx
@@ -0,0 +1,81 @@
+import React from "react";
+
+function About() {
+  return (
+    <div
+      name="About"
+      className="max-w-screen-2xl container mx-auto px-4 md:px-20 my-20"
+    >
+      <div>
+        <h1 className="text-3xl font-bold mb-5">About</h1>
+        <p>
+          Hello, I'm Swapnil, a passionate Web developer with a keen eye for
+          MERN Stack . With a background in IT, I strive to create impactful and
+          visually stunning Software solutions that leave a lasting impression.
+        </p>
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Education & Training
+        </h1>
+        <span>
+          I am Swapnil Takalkar. I completed my Bachelor of Engineering at JSPM
+          Narhe Technical Campus in 2022. Prior to this, I earned a Diploma from
+          Rajarshi Shau College of Polytechnic in 2019. To further enhance my
+          skills, I completed Python Training from Internshala in 2020 and React
+          JS Training from Udemy in 2023.
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Skills & Expertise
+        </h1>
+        <span>
+          I am proficient in Java, C#, .NET, C++, HTML, CSS, JavaScript,
+          ReactJS, Angular, NodeJS, SQL, and MongoDB. I have experience with
+          REST API, MySQL, SQL Server, ExpressJS, and GIT. I possess a strong
+          grasp of Object-Oriented Programming (OOP) principles, excellent
+          problem-solving skills, and am an effective communicator and
+          collaborator.
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Professional Experience
+        </h1>
+        <span>
+          As a Web Developer Intern at Radiant IT Services Pvt. Ltd. from
+          October 2023 to March 2024, I contributed to real-world projects,
+          aiding in their successful completion and resulting in a 20% increase
+          in project efficiency. I improved software stability by debugging and
+          troubleshooting, and enhanced code quality through rigorous code
+          reviews.
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Achievements & Awards
+        </h1>
+        <span>
+          I presented a paper at the National Techno-Social Symposium.
+          Additionally, I participated in a quiz competition at R-Revolution
+          Dahsham. I also engaged in a state-level workshop on automotive
+          product design organized by Asia Tech.
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Mission Statement
+        </h1>
+        <p>
+          My mission is to leverage my skills and creativity to deliver
+          innovative MERN stack solutions that exceed client expectations and
+          contribute positively to the digital landscape. I am committed to
+          continuous learning and growth, always seeking new challenges and
+          opportunities to expand my horizons.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default About;
